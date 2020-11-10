e69b5874e919e414e69d5bdcf2f77311fe940857
Grant Hesketh <ghesketh76@gmail.com>
2020-11-10 21:16:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@
 def reverse_each_word (string)
   origional_array = string.split(" ")
   return_array = []
-  origional_array.each do |string|
+  origional_array.collect do |string|
     return_array << string.reverse
   end
   return_array.join(" ")
